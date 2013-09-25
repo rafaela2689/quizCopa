c775f46766980698e29d23a83aba5ed48d8a6f7c
rafaela2689 <rafacavalcante.rca@gmail.com>
2013-09-24 21:13:09 -0400

    Criação das classes de modelo.

---
+++ AppQuizCopa/src/com/android/quiz/model/Question.java
@@ -0,0 +1,54 @@
+package com.android.quiz.model;
+
+public class Question {
+	
+	private String enunciado;
+	private String opcao1;
+	private String opcao2;
+	private String opcao3;
+	private String opcao4;
+	private String resposta;
+	
+	
+	public String getEnunciado() {
+		return enunciado;
+	}
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+	public String getOpcao1() {
+		return opcao1;
+	}
+	public void setOpcao1(String opcao1) {
+		this.opcao1 = opcao1;
+	}
+	public String getOpcao2() {
+		return opcao2;
+	}
+	public void setOpcao2(String opcao2) {
+		this.opcao2 = opcao2;
+	}
+	public String getOpcao3() {
+		return opcao3;
+	}
+	public void setOpcao3(String opcao3) {
+		this.opcao3 = opcao3;
+	}
+	public String getOpcao4() {
+		return opcao4;
+	}
+	public void setOpcao4(String opcao4) {
+		this.opcao4 = opcao4;
+	}
+	public String getResposta() {
+		return resposta;
+	}
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+	
+	
+	
+	
+
+}
